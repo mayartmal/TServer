73b72d974f863e09b7baf3a0f55e714100d8e1b0
mayartmal <mayartmal@gmail.com>
2024-12-30 19:26:44 +0400

    add pages logic

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import socket
+from http.client import responses
 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("127.0.0.1", 2000)) #>1024 port
-server.listen(4)
-client_socket, address = server.accept()
-data = client_socket.recv(1024).decode('utf-8')
-print(data)
-HDRS = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n"
-content = "It works".encode("utf-8")
-client_socket.send(HDRS.encode("utf-8") + content)
-print("Shut down it")
+def start_my_server():
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(("127.0.0.1", 2000)) #>1024 port
+        server.listen(4)
+        while True:
+            print("Working...")
+            client_socket, address = server.accept()
+            data = client_socket.recv(1024).decode('utf-8')
+            content = load_page_from_get_requests(data)
+            client_socket.send(content)
+            client_socket.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        server.close()
+        print("Shut down the socket")
+
+
+
+def load_page_from_get_requests(request_data):
+    HDRS = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n"
+    path = request_data.split(" ")[1]
+    response = ''
+    with open('views'+path, 'rb') as file:
+        response = file.read()
+    return HDRS.encode('utf-8') + response
+
+if __name__ == "__main__":
+    start_my_server()
